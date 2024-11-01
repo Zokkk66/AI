b6b55440294a3c996503d86b75e0b7d299ed5d96
佐罗斯 <136051963+Zokkk66@users.noreply.github.com>
2024-11-02 01:51:02 +0800

    Update script.js

--- script.js
+++ script.js
@@ -87,3 +87,12 @@ window.addEventListener('click', function (event) {
         closeModal();
     }
 });
+
+// 字体变色功能
+const title = document.getElementById('color-changing-title');
+const colors = ['red', 'blue', 'green', 'yellow', 'purple'];
+let index = 0;
+setInterval(function () {
+    title.style.color = colors[index];
+    index = (index + 1) % colors.length;
+}, 1000);
